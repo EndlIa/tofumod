876c553819fe303c0dce513a465b0ac5c482bf2a
Ednlia <2017604293@qq.com>
2024-06-05 15:24:07 +0800

    a commit

--- src/main/java/moe/endlia/Tofumod.java
+++ src/main/java/moe/endlia/Tofumod.java
@@ -6,11 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tofumod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("tofumod");
 
+	public static final String MOD_ID = "tofumod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
