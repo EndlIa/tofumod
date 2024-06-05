8ed75fd4f5d8f227242739817f042d558254dc16
Ednlia <2017604293@qq.com>
2024-06-05 14:54:15 +0800

    Initial commit

---
+++ src/main/java/moe/endlia/TofumodClient.java
@@ -0,0 +1,11 @@
+package moe.endlia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TofumodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/moe/endlia/TofumodDataGenerator.java
@@ -0,0 +1,11 @@
+package moe.endlia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TofumodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
