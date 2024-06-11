433059a397173c027f2619ac7df6b70a138f3636
Ednlia <2017604293@qq.com>
2024-06-11 22:19:54 +0800

    Add tofu gem and ore

--- src/main/java/moe/endlia/block/ModBlocks.java
+++ src/main/java/moe/endlia/block/ModBlocks.java
@@ -1,24 +1,32 @@
 package moe.endlia.block;
 
 import moe.endlia.Tofumod;
+
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
+
     public static final Block MOMEN_TOFU_BLOCK = registerBlock("momen_tofu_block",
-            new Block(AbstractBlock.Settings.create().strength(4.0f)));
+            new Block(AbstractBlock.Settings.create().strength(1.0f)));
+    public static final Block TOFU_ORE_BLOCK = registerBlock("tofu_ore_block",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.copy(Blocks.COAL_ORE)));
+
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(Tofumod.MOD_ID, name), block);
     }
-
     private static Item registerBlockItem(String name, Block block) {
         return Registry.register(Registries.ITEM, new Identifier(Tofumod.MOD_ID, name),
                 new BlockItem(block, new Item.Settings()));
--- src/main/java/moe/endlia/item/ModItems.java
+++ src/main/java/moe/endlia/item/ModItems.java
@@ -12,6 +12,7 @@ public class ModItems {
 
     public static final Item MOMEN_TOFU = registerItem("momen_tofu", new Item(new Item.Settings()));
     public static final Item KINUGOSHI_TOFU = registerItem("kinugoshi_tofu", new Item(new Item.Settings()));
+    public static final Item TOFU_GEM = registerItem("tofu_gem", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Tofumod.MOD_ID, name), item);
